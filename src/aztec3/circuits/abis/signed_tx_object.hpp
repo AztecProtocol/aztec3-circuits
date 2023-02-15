8fc156beb42925a2daebac17db3921ba0a6707f0
dbanks12 <david@aztecprotocol.com>
2023-02-15 00:28:31 +0000

    rename tx_object -> tx_request

--- src/aztec3/circuits/abis/signed_tx_object.hpp
+++ src/aztec3/circuits/abis/signed_tx_object.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "tx_object.hpp"
+#include "tx_request.hpp"
 
 #include <stdlib/primitives/witness/witness.hpp>
 #include <stdlib/types/native_types.hpp>
@@ -13,8 +13,8 @@ using plonk::stdlib::witness_t;
 using plonk::stdlib::types::CircuitTypes;
 using plonk::stdlib::types::NativeTypes;
 
-template <typename NCT> struct SignedTxObject {
-    TxObject<NCT> tx_object;
+template <typename NCT> struct SignedTxRequest {
+    TxRequest<NCT> tx_request;
     // Signature<NCT> signature; // TODO: import some kind of signature
 
     template <typename Composer> SignedTxObject<CircuitTypes<Composer>> to_circuit_type(Composer& composer) const
@@ -25,31 +25,31 @@ template <typename NCT> struct SignedTxObject {
         // auto to_ct = [&](auto& e) { return plonk::stdlib::types::to_ct(composer, e); };
         auto to_circuit_type = [&](auto& e) { return e.to_circuit_type(composer); };
 
-        SignedTxObject<CircuitTypes<Composer>> signed_tx_object = {
-            to_circuit_type(tx_object),
+        SignedTxRequest<CircuitTypes<Composer>> signed_tx_request = {
+            to_circuit_type(tx_request),
         };
 
-        return signed_tx_object;
+        return signed_tx_request;
     };
 };
 
-template <typename NCT> void read(uint8_t const*& it, SignedTxObject<NCT>& signed_tx_object)
+template <typename NCT> void read(uint8_t const*& it, SignedTxRequest<NCT>& signed_tx_request)
 {
     using serialize::read;
 
-    read(it, signed_tx_object.tx_object);
+    read(it, signed_tx_request.tx_request);
 };
 
-template <typename NCT> void write(std::vector<uint8_t>& buf, SignedTxObject<NCT> const& signed_tx_object)
+template <typename NCT> void write(std::vector<uint8_t>& buf, SignedTxRequest<NCT> const& signed_tx_request)
 {
     using serialize::write;
 
-    write(buf, signed_tx_object.tx_object);
+    write(buf, signed_tx_request.tx_request);
 };
 
-template <typename NCT> std::ostream& operator<<(std::ostream& os, SignedTxObject<NCT> const& signed_tx_object)
+template <typename NCT> std::ostream& operator<<(std::ostream& os, SignedTxRequest<NCT> const& signed_tx_request)
 {
-    return os << "tx_object: " << signed_tx_object.tx_object << "\n";
+    return os << "tx_request: " << signed_tx_request.tx_request << "\n";
 }
 
 } // namespace aztec3::circuits::abis
--- src/aztec3/circuits/abis/tx_object.hpp
+++ src/aztec3/circuits/abis/tx_object.hpp
@@ -26,7 +26,7 @@ template <typename NCT> struct TxObject {
     TxContext<NCT> tx_context;
     fr chain_id;
 
-    template <typename Composer> TxObject<CircuitTypes<Composer>> to_circuit_type(Composer& composer) const
+    template <typename Composer> TxRequest<CircuitTypes<Composer>> to_circuit_type(Composer& composer) const
     {
         static_assert((std::is_same<NativeTypes, NCT>::value));
 
@@ -34,51 +34,51 @@ template <typename NCT> struct TxObject {
         auto to_ct = [&](auto& e) { return plonk::stdlib::types::to_ct(composer, e); };
         auto to_circuit_type = [&](auto& e) { return e.to_circuit_type(composer); };
 
-        TxObject<CircuitTypes<Composer>> tx_object = {
+        TxRequest<CircuitTypes<Composer>> tx_request = {
             to_ct(from),     to_ct(to),    to_circuit_type(function_signature),
             to_ct(args),     to_ct(nonce), to_circuit_type(tx_context),
             to_ct(chain_id),
         };
 
-        return tx_object;
+        return tx_request;
     };
 };
 
-template <typename NCT> void read(uint8_t const*& it, TxObject<NCT>& tx_object)
+template <typename NCT> void read(uint8_t const*& it, TxRequest<NCT>& tx_request)
 {
     using serialize::read;
 
-    read(it, tx_object.from);
-    read(it, tx_object.to);
-    read(it, tx_object.function_signature);
-    read(it, tx_object.args);
-    read(it, tx_object.nonce);
-    read(it, tx_object.tx_context);
-    read(it, tx_object.chain_id);
+    read(it, tx_request.from);
+    read(it, tx_request.to);
+    read(it, tx_request.function_signature);
+    read(it, tx_request.args);
+    read(it, tx_request.nonce);
+    read(it, tx_request.tx_context);
+    read(it, tx_request.chain_id);
 };
 
-template <typename NCT> void write(std::vector<uint8_t>& buf, TxObject<NCT> const& tx_object)
+template <typename NCT> void write(std::vector<uint8_t>& buf, TxRequest<NCT> const& tx_request)
 {
     using serialize::write;
 
-    write(buf, tx_object.from);
-    write(buf, tx_object.to);
-    write(buf, tx_object.function_signature);
-    write(buf, tx_object.args);
-    write(buf, tx_object.nonce);
-    write(buf, tx_object.tx_context);
-    write(buf, tx_object.chain_id);
+    write(buf, tx_request.from);
+    write(buf, tx_request.to);
+    write(buf, tx_request.function_signature);
+    write(buf, tx_request.args);
+    write(buf, tx_request.nonce);
+    write(buf, tx_request.tx_context);
+    write(buf, tx_request.chain_id);
 };
 
-template <typename NCT> std::ostream& operator<<(std::ostream& os, TxObject<NCT> const& tx_object)
+template <typename NCT> std::ostream& operator<<(std::ostream& os, TxRequest<NCT> const& tx_request)
 {
-    return os << "from: " << tx_object.from << "\n"
-              << "to: " << tx_object.to << "\n"
-              << "function_signature: " << tx_object.function_signature << "\n"
-              << "args: " << tx_object.args << "\n"
-              << "nonce: " << tx_object.nonce << "\n"
-              << "tx_context: " << tx_object.tx_context << "\n"
-              << "chain_id: " << tx_object.chain_id << "\n";
+    return os << "from: " << tx_request.from << "\n"
+              << "to: " << tx_request.to << "\n"
+              << "function_signature: " << tx_request.function_signature << "\n"
+              << "args: " << tx_request.args << "\n"
+              << "nonce: " << tx_request.nonce << "\n"
+              << "tx_context: " << tx_request.tx_context << "\n"
+              << "chain_id: " << tx_request.chain_id << "\n";
 }
 
 } // namespace aztec3::circuits::abis
