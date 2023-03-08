7223057607ab84d929542baebced2da3b9b33141
dbanks12 <david@aztecprotocol.com>
2023-03-08 01:23:17 +0000

    add CMAKE_BBERG_PRESET cacheVariable that is used to set barretenberg's preset from this project. Fix gitignores

--- cpp/cmake/barretenberg.cmake
+++ cpp/cmake/barretenberg.cmake
@@ -9,34 +9,10 @@
 
 include(ExternalProject)
 
-if (WASM)
-    set(BBERG_BUILD_DIR ${BBERG_DIR}/build-wasm)
-else()
-    set(BBERG_BUILD_DIR ${BBERG_DIR}/build)
+if(NOT CMAKE_BBERG_PRESET)
+    set(CMAKE_BBERG_PRESET default)
 endif()
 
-# If the OpenMP library is included via this option, propogate to ExternalProject configure
-if (OpenMP_omp_LIBRARY)
-    set(LIB_OMP_OPTION -DOpenMP_omp_LIBRARY=${OpenMP_omp_LIBRARY})
-endif()
-
-# Make sure barretenberg doesn't set its own WASI_SDK_PREFIX
-if (WASI_SDK_PREFIX)
-    set(WASI_SDK_OPTION -DWASI_SDK_PREFIX=${WASI_SDK_PREFIX})
-endif()
-
-# cmake configure cli args for ExternalProject
-set(BBERG_CONFIGURE_ARGS -DTOOLCHAIN=${TOOLCHAIN} ${WASI_SDK_OPTION} ${LIB_OMP_OPTION} -DCI=${CI} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
-
-message(STATUS "CMAKE_PRESET: " ${CMAKE_PRESET})
-#list(FIND CMAKE_USER_ARGS "--preset" preset_index)
-#message(STATUS "preset_index: " ${preset_index})
-#list(GET CMAKE_USER_ARGS ${preset_index} preset_value_index)
-#math(EXPR preset_value_index "${preset_value_index} + 1")
-#message(STATUS "preset_value_index: " ${preset_value_index})
-#list(GET CMAKE_USER_ARGS ${preset_value_index} preset_value)
-#message(STATUS "preset_value: " ${preset_value})
-message(STATUS "GENERATOR_PRESET: " ${CMAKE_GENERATOR_TOOLSET})
 # Naming: Project: Barretenberg, Libraries: barretenberg, env
 # Need BUILD_ALWAYS to ensure that barretenberg is automatically reconfigured when its CMake files change
 # "Enabling this option forces the build step to always be run. This can be the easiest way to robustly
@@ -44,12 +20,12 @@ message(STATUS "GENERATOR_PRESET: " ${CMAKE_GENERATOR_TOOLSET})
 #  default success timestamp-based method." - https://cmake.org/cmake/help/latest/module/ExternalProject.html
 ExternalProject_Add(Barretenberg
     SOURCE_DIR ${BBERG_DIR}
-    BINARY_DIR ${BBERG_BUILD_DIR} # build directory
+    BUILD_IN_SOURCE TRUE
     BUILD_ALWAYS TRUE
     UPDATE_COMMAND ""
-    INSTALL_COMMAND "")
-    #CONFIGURE_COMMAND cd .. && ${CMAKE_COMMAND} --preset ${CMAKE_PRESET} ${BBERG_CONFIGURE_ARGS}
-    #BUILD_COMMAND cd .. && ${CMAKE_COMMAND} --build --preset ${CMAKE_PRESET} --parallel --target barretenberg --target env)
+    INSTALL_COMMAND ""
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} --preset ${CMAKE_BBERG_PRESET}
+    BUILD_COMMAND ${CMAKE_COMMAND} --build --preset ${CMAKE_BBERG_PRESET} --target barretenberg --target env)
 
 include_directories(${BBERG_DIR}/src/aztec)
 
