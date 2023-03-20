9cba121a3c978bd9975ddb670c1aa53ed1cce405
spalladino <santiago@aztecprotocol.com>
2023-03-20 12:19:07 +0000

    Use values provided by Charlie for constants

--- cpp/src/aztec3/constants.hpp
+++ cpp/src/aztec3/constants.hpp
@@ -20,6 +20,7 @@ constexpr size_t L1_MSG_STACK_LENGTH = 2;
 
 constexpr size_t KERNEL_NEW_COMMITMENTS_LENGTH = 16;
 constexpr size_t KERNEL_NEW_NULLIFIERS_LENGTH = 16;
+constexpr size_t KERNEL_NEW_CONTRACTS_LENGTH = 8;
 constexpr size_t KERNEL_PRIVATE_CALL_STACK_LENGTH = 8;
 constexpr size_t KERNEL_PUBLIC_CALL_STACK_LENGTH = 8;
 constexpr size_t KERNEL_L1_MSG_STACK_LENGTH = 4;
@@ -29,6 +30,9 @@ constexpr size_t VK_TREE_HEIGHT = 3;
 constexpr size_t CONTRACT_TREE_HEIGHT = 4;
 constexpr size_t PRIVATE_DATA_TREE_HEIGHT = 8;
 constexpr size_t NULLIFIER_TREE_HEIGHT = 8;
+
+constexpr size_t PRIVATE_DATA_TREE_ROOTS_TREE_HEIGHT = 8;
+constexpr size_t CONTRACT_TREE_ROOTS_TREE_HEIGHT = 8;
 
 constexpr size_t FUNCTION_SELECTOR_NUM_BYTES = 31; // must be <= 31
 
