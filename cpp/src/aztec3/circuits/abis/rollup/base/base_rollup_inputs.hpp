2a500fd36062ffc0a81149b6c894d1d87fc29251
cheethas <urmasurda@gmail.com>
2023-03-27 20:58:53 +0000

    fix(base): rename messup

--- cpp/src/aztec3/circuits/abis/rollup/base/base_rollup_inputs.hpp
+++ cpp/src/aztec3/circuits/abis/rollup/base/base_rollup_inputs.hpp
@@ -11,7 +11,7 @@ using aztec3::utils::types::CircuitTypes;
 using aztec3::utils::types::NativeTypes;
 using std::is_same;
 
-template <typename NCT> struct MergeRollupInputs {
+template <typename NCT> struct BaseRollupInputs {
 
     typedef typename NCT::fr fr;
 
@@ -31,10 +31,10 @@ template <typename NCT> struct MergeRollupInputs {
 
     fr prover_id;
 
-    bool operator==(MergeRollupInputs<NCT> const&) const = default;
+    bool operator==(BaseRollupInputs<NCT> const&) const = default;
 };
 
-template <typename NCT> void read(uint8_t const*& it, MergeRollupInputs<NCT>& obj)
+template <typename NCT> void read(uint8_t const*& it, BaseRollupInputs<NCT>& obj)
 {
     using serialize::read;
 
@@ -48,7 +48,7 @@ template <typename NCT> void read(uint8_t const*& it, MergeRollupInputs<NCT>& ob
     read(it, obj.prover_id);
 };
 
-template <typename NCT> void write(std::vector<uint8_t>& buf, MergeRollupInputs<NCT> const& obj)
+template <typename NCT> void write(std::vector<uint8_t>& buf, BaseRollupInputs<NCT> const& obj)
 {
     using serialize::write;
 
@@ -62,7 +62,7 @@ template <typename NCT> void write(std::vector<uint8_t>& buf, MergeRollupInputs<
     write(buf, obj.prover_id);
 };
 
-template <typename NCT> std::ostream& operator<<(std::ostream& os, MergeRollupInputs<NCT> const& obj)
+template <typename NCT> std::ostream& operator<<(std::ostream& os, BaseRollupInputs<NCT> const& obj)
 {
     return os << "kernel_data:\n"
               << obj.kernel_data << "\n"
