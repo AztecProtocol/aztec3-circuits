f9db8955bb6af39711bf3e6eadcfebb07dab6b0c
cheethas <urmasurda@gmail.com>
2023-03-27 16:05:19 +0000

    feat(base_rollup): scaffold

--- cpp/src/aztec3/circuits/rollup/base/native_base_rollup_circuit.hpp
+++ cpp/src/aztec3/circuits/rollup/base/native_base_rollup_circuit.hpp
@@ -2,14 +2,7 @@
 
 #include "init.hpp"
 
-#include <aztec3/oracle/oracle.hpp>
-#include <aztec3/circuits/apps/oracle_wrapper.hpp>
-#include <aztec3/circuits/apps/function_execution_context.hpp>
-
-#include <aztec3/circuits/recursion/aggregator.hpp>
-
-#include <aztec3/circuits/abis/private_kernel/private_inputs.hpp>
-
+// TODO: not needed right at this moment for native impl
 #include <barretenberg/stdlib/types/types.hpp>
 #include <aztec3/utils/types/convert.hpp>
 #include <aztec3/utils/types/circuit_types.hpp>
@@ -22,20 +15,6 @@
 
 namespace aztec3::circuits::rollup::native_base_rollup {
 
-// Turbo specific, at the moment:
-using Composer = plonk::stdlib::types::Composer;
-using plonk::stdlib::types::Prover;
-
-using Aggregator = aztec3::circuits::recursion::Aggregator;
-
-// Generic:
-using CT = aztec3::utils::types::CircuitTypes<Composer>;
-using NT = aztec3::utils::types::NativeTypes;
-using aztec3::utils::types::to_ct;
-
-// TODO: not sure if we need this one
-using FunctionExecutionContext = aztec3::circuits::apps::FunctionExecutionContext<Composer>;
-
-void base_rollup_circuit(ConstantBaseRollupData<NT> constantRollupData, BaseRollupInputs<NT> baseRollupInputs);
+void base_rollup_circuit(ConstantBaseRollupData constantRollupData, BaseRollupInputs baseRollupInputs);
 
 } // namespace aztec3::circuits::rollup::native_base_rollup
