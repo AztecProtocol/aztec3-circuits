38cd9ca65d358b76d589be810f3253324912173f
cheethas <urmasurda@gmail.com>
2023-03-27 18:29:25 +0000

    clean(merge): clean types and scaffold

--- cpp/src/aztec3/circuits/rollup/merge/native_merge_rollup_circuit.hpp
+++ cpp/src/aztec3/circuits/rollup/merge/native_merge_rollup_circuit.hpp
@@ -8,13 +8,10 @@
 #include <aztec3/utils/types/circuit_types.hpp>
 #include <aztec3/utils/types/native_types.hpp>
 
-#include <aztec3/circuits/abis/merge_rollup/base_rollup_inputs.hpp>
-#include <aztec3/circuits/abis/merge_rollup/base_rollup_public_inputs.hpp>
-#include <aztec3/circuits/abis/merge_rollup/nullifier_leaf_preimage.hpp>
-#include <aztec3/circuits/abis/merge_rollup/constant_base_rollup_data.hpp>
+#include <aztec3/circuits/abis/merge_rollup/merge_rollup_inputs.hpp>
 
 namespace aztec3::circuits::rollup::native_merge_rollup {
 
-void merge_rollup_circuit(ConstantMergeRollupData constantMergeRollupData, MergeRollupInputs mergeRollupInputs);
+void merge_rollup_circuit(MergeRollupInputs mergeRollupInputs);
 
 } // namespace aztec3::circuits::rollup::native_merge_rollup
