ab554163c3872d2254be6af51275e66fba47ec2f
dbanks12 <david@aztecprotocol.com>
2023-03-28 16:13:22 +0000

    update kernel interfaces with sim

--- cpp/src/aztec3/circuits/kernel/private/c_bind.cpp
+++ cpp/src/aztec3/circuits/kernel/private/c_bind.cpp
@@ -2,6 +2,7 @@
 #include "init.hpp"
 #include "c_bind.h"
 
+// TODO remove these? present in init/index?
 #include <aztec3/constants.hpp>
 #include <aztec3/utils/types/native_types.hpp>
 #include "aztec3/circuits/abis/signed_tx_request.hpp"
@@ -28,6 +29,31 @@ using aztec3::circuits::mock::mock_kernel_circuit;
 
 using plonk::TurboComposer;
 using namespace plonk::stdlib::types;
+
+PreviousKernelData<NT> default_previous_kernel()
+{
+    // TODO confirm this is the right way to initialize struct of 0s
+    auto mock_kernel_public_inputs = PublicInputs<NT>();
+    mock_kernel_public_inputs.end.private_call_stack =
+        std::array<NT::fr, aztec3::KERNEL_PRIVATE_CALL_STACK_LENGTH>({ { 0 } }),
+    mock_kernel_public_inputs.is_private = true;
+
+    auto crs_factory = std::make_shared<EnvReferenceStringFactory>();
+    Composer mock_kernel_composer = Composer(crs_factory);
+    mock_kernel_circuit(mock_kernel_composer, mock_kernel_public_inputs);
+
+    plonk::stdlib::types::Prover mock_kernel_prover = mock_kernel_composer.create_prover();
+    NT::Proof mock_kernel_proof = mock_kernel_prover.construct_proof();
+
+    std::shared_ptr<NT::VK> mock_kernel_vk = mock_kernel_composer.compute_verification_key();
+
+    PreviousKernelData<NT> previous_kernel = {
+        .public_inputs = mock_kernel_public_inputs,
+        .proof = mock_kernel_proof,
+        .vk = mock_kernel_vk,
+    };
+    return previous_kernel;
+}
 } // namespace
 
 #define WASM_EXPORT __attribute__((visibility("default")))
@@ -63,16 +89,55 @@ WASM_EXPORT size_t private_kernel__init_verification_key(uint8_t const* pk_buf, 
 
 // TODO comment about how public_inputs is a confusing name
 // returns size of public inputs
-WASM_EXPORT size_t private_kernel__create_proof(uint8_t const* signed_tx_request_buf,
-                                                uint8_t const* previous_kernel_buf,
-                                                uint8_t const* private_call_buf,
-                                                uint8_t const* pk_buf,
-                                                bool proverless,
-                                                uint8_t const** proof_data_buf,
-                                                size_t* proof_data_size,
-                                                uint8_t const** private_kernel_public_inputs_buf)
+WASM_EXPORT size_t private_kernel__sim(uint8_t const* signed_tx_request_buf,
+                                       uint8_t const* previous_kernel_buf,
+                                       uint8_t const* private_call_buf,
+                                       bool first_iteration,
+                                       uint8_t const** private_kernel_public_inputs_buf)
 {
-    info(previous_kernel_buf);
+    SignedTxRequest<NT> signed_tx_request;
+    read(signed_tx_request_buf, signed_tx_request);
+
+    PrivateCallData<NT> private_call_data;
+    read(private_call_buf, private_call_data);
+
+    PreviousKernelData<NT> previous_kernel;
+    if (first_iteration) {
+        previous_kernel = default_previous_kernel();
+    } else {
+        read(previous_kernel_buf, previous_kernel);
+    }
+
+    PrivateInputs<NT> private_inputs = PrivateInputs<NT>{
+        .signed_tx_request = signed_tx_request,
+        .previous_kernel = default_previous_kernel(),
+        .private_call = private_call_data,
+
+    };
+
+    PublicInputs<NT> public_inputs;
+
+    public_inputs = private_kernel_native(private_inputs);
+
+    // serialize public inputs to bytes vec
+    std::vector<uint8_t> public_inputs_vec;
+    write(public_inputs_vec, public_inputs);
+    // copy public inputs to output buffer
+    auto raw_public_inputs_buf = (uint8_t*)malloc(public_inputs_vec.size());
+    memcpy(raw_public_inputs_buf, (void*)public_inputs_vec.data(), public_inputs_vec.size());
+    *private_kernel_public_inputs_buf = raw_public_inputs_buf;
+
+    return public_inputs_vec.size();
+}
+
+// returns size of proof data
+WASM_EXPORT size_t private_kernel__prove(uint8_t const* signed_tx_request_buf,
+                                         uint8_t const* previous_kernel_buf,
+                                         uint8_t const* private_call_buf,
+                                         uint8_t const* pk_buf,
+                                         bool first_iteration,
+                                         uint8_t const** proof_data_buf)
+{
     // TODO accept proving key and use that to initialize composers
     // this info is just to prevent error for unused pk_buf
     // TODO do we want to accept it or just get it from our factory?
@@ -85,75 +150,39 @@ WASM_EXPORT size_t private_kernel__create_proof(uint8_t const* signed_tx_request
     PrivateCallData<NT> private_call_data;
     read(private_call_buf, private_call_data);
 
-    std::array<NT::fr, aztec3::KERNEL_PRIVATE_CALL_STACK_LENGTH> initial_kernel_private_call_stack{};
-    initial_kernel_private_call_stack[0] = private_call_data.call_stack_item.hash();
-
-    auto mock_kernel_public_inputs = PublicInputs<NT>();
-    mock_kernel_public_inputs.end.private_call_stack = initial_kernel_private_call_stack,
-    // TODO who should inject this? C++ or cbind?
-        mock_kernel_public_inputs.constants.old_tree_roots.private_data_tree_root =
-            private_call_data.call_stack_item.public_inputs.historic_private_data_tree_root;
-    mock_kernel_public_inputs.constants.tx_context = signed_tx_request.tx_request.tx_context;
-    mock_kernel_public_inputs.is_private = true;
-
-    // FIXME composer doesn't work in wasm
-    Composer mock_kernel_composer = Composer(crs_factory);
-    mock_kernel_circuit(mock_kernel_composer, mock_kernel_public_inputs);
-
-    plonk::stdlib::types::Prover mock_kernel_prover = mock_kernel_composer.create_prover();
-    NT::Proof mock_kernel_proof = mock_kernel_prover.construct_proof();
-
-    std::shared_ptr<NT::VK> mock_kernel_vk = mock_kernel_composer.compute_verification_key();
-
+    PreviousKernelData<NT> previous_kernel;
+    if (first_iteration) {
+        previous_kernel = default_previous_kernel();
+    } else {
+        read(previous_kernel_buf, previous_kernel);
+    }
     PrivateInputs<NT> private_inputs = PrivateInputs<NT>{
         .signed_tx_request = signed_tx_request,
-        .previous_kernel =
-            PreviousKernelData<NT>{
-                .public_inputs = mock_kernel_public_inputs,
-                .proof = mock_kernel_proof,
-                .vk = mock_kernel_vk,
-            },
+        .previous_kernel = default_previous_kernel(),
         .private_call = private_call_data,
-
     };
 
-    NT::Proof private_kernel_proof;
+    Composer private_kernel_composer = Composer(crs_factory);
+    plonk::stdlib::types::Prover private_kernel_prover = private_kernel_composer.create_prover();
+
     PublicInputs<NT> public_inputs;
-    if (proverless) {
-        public_inputs = private_kernel_native(private_inputs);
-        // mocked proof - zeros
-        private_kernel_proof = NT::Proof{ std::vector<uint8_t>(42, 0) };
-    } else {
-        Composer private_kernel_composer = Composer(crs_factory);
-        plonk::stdlib::types::Prover private_kernel_prover = private_kernel_composer.create_prover();
-        public_inputs = private_kernel_circuit(private_kernel_composer, private_inputs);
-        private_kernel_proof = private_kernel_prover.construct_proof();
-    }
+    public_inputs = private_kernel_circuit(private_kernel_composer, private_inputs);
+    NT::Proof private_kernel_proof;
+    private_kernel_proof = private_kernel_prover.construct_proof();
 
     // copy proof data to output buffer
     auto raw_proof_buf = (uint8_t*)malloc(private_kernel_proof.proof_data.size());
     memcpy(raw_proof_buf, (void*)private_kernel_proof.proof_data.data(), private_kernel_proof.proof_data.size());
     *proof_data_buf = raw_proof_buf;
 
-    // copy proof data size to output
-    *proof_data_size = private_kernel_proof.proof_data.size();
-
-    // serialize public inputs to bytes vec
-    std::vector<uint8_t> public_inputs_vec;
-    write(public_inputs_vec, public_inputs);
-    // copy public inputs to output buffer
-    auto raw_public_inputs_buf = (uint8_t*)malloc(public_inputs_vec.size());
-    memcpy(raw_public_inputs_buf, (void*)public_inputs_vec.data(), public_inputs_vec.size());
-    *private_kernel_public_inputs_buf = raw_public_inputs_buf;
-
     return private_kernel_proof.proof_data.size();
 }
 
 WASM_EXPORT size_t private_kernel__verify_proof(uint8_t const* vk_buf, uint8_t const* proof, uint32_t length)
 {
-    info(vk_buf);
-    info(proof);
-    info(length);
+    (void)vk_buf; // unused
+    (void)proof;  // unused
+    (void)length; // unused
     return true;
 }
 
